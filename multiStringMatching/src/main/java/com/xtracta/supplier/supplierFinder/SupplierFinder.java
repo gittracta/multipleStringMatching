df5e3368cbbf020aea3533874851cc610931afcd
Firas Swidan <firas.swidan@protonmail.com>
2016-04-26 12:05:33 +1200

    implemented supplier finder

--- multiStringMatching/src/main/java/com/xtracta/supplier/supplierFinder/SupplierFinder.java
+++ multiStringMatching/src/main/java/com/xtracta/supplier/supplierFinder/SupplierFinder.java
@@ -1,5 +1,37 @@
 package com.xtracta.supplier.supplierFinder;
 
-public class SupplierFinder {
+import java.io.IOException;
+import java.util.ArrayList;
 
+import org.junit.Assert;
+
+import com.xtracta.supplier.interfaceUtils.InterfaceFactory;
+import com.xtracta.supplier.interfaceUtils.Invoice;
+import com.xtracta.supplier.interfaceUtils.Suppliers;
+import com.xtracta.supplier.multipleStringMatching.AhoCorasickMultipleStringMatchingRobertBorImpl;
+
+public class SupplierFinder {
+	
+	public String findSupplier(String invoiceFile, String supplierFile) throws IOException{
+		AhoCorasickMultipleStringMatchingRobertBorImpl ahoCor = new AhoCorasickMultipleStringMatchingRobertBorImpl();
+
+		Suppliers suppliers = InterfaceFactory.getDefaultSuppliers(supplierFile);
+		Invoice invoice = InterfaceFactory.getDetaultInvoice(invoiceFile);
+
+		while(suppliers.hasNext()){
+			ahoCor.addKeyword(suppliers.getNextSupplierName());
+		}
+		ahoCor.buildTrie();
+
+		ArrayList<String> invoiceContent = invoice.parse();
+
+		for(String string : invoiceContent){
+			String match = null;
+			if( (match = ahoCor.findMatch(string)) != null ){
+				return match;
+			}
+		}
+		return null;
+			
+	}
 }
